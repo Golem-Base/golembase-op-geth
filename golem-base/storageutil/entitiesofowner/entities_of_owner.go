90e995ccd1511ab372c66cc8aad4edd36ad05ffd
Dragan Milic <dragan@netice9.com>
2025-03-28 23:10:48 +0100

    fix issue 'new entry in the list is not zero in storage'

--- golem-base/storageutil/entitiesofowner/entities_of_owner.go
+++ golem-base/storageutil/entitiesofowner/entities_of_owner.go
@@ -5,6 +5,7 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/golem-base/storageutil"
 	"github.com/ethereum/go-ethereum/golem-base/storageutil/keyset"
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/holiman/uint256"
 )
 
@@ -12,12 +13,24 @@ type StateAccess = storageutil.StateAccess
 
 var OwnerEntitiesSalt = []byte("golemBase.allEntities")
 
-func AddEntity(db StateAccess, owner common.Address, entity common.Hash) error {
+func AddEntity(db StateAccess, owner common.Address, entity common.Hash) (err error) {
+	defer func() {
+		if err != nil {
+			log.Error("Error adding entity", "owner", owner, "entity", entity, "error", err)
+		}
+	}()
+	log.Info("Adding entity", "owner", owner, "entity", entity)
 	ownerKey := crypto.Keccak256Hash(OwnerEntitiesSalt, owner.Bytes())
 	return keyset.AddValue(db, ownerKey, entity)
 }
 
-func RemoveEntity(db StateAccess, owner common.Address, entity common.Hash) error {
+func RemoveEntity(db StateAccess, owner common.Address, entity common.Hash) (err error) {
+	defer func() {
+		if err != nil {
+			log.Error("Error removing entity", "owner", owner, "entity", entity, "error", err)
+		}
+	}()
+	log.Info("Removing entity", "owner", owner, "entity", entity)
 	ownerKey := crypto.Keccak256Hash(OwnerEntitiesSalt, owner.Bytes())
 	return keyset.RemoveValue(db, ownerKey, entity)
 }
