bc85bd6039047e60abe1e523413c0a32d4a72db8
Dragan Milic <dragan@netice9.com>
2025-05-22 13:09:12 +0200

    cli: added listing of entities

--- cmd/golembase/entity/entity.go
+++ cmd/golembase/entity/entity.go
@@ -3,6 +3,7 @@ package entity
 import (
 	"github.com/ethereum/go-ethereum/cmd/golembase/entity/create"
 	"github.com/ethereum/go-ethereum/cmd/golembase/entity/delete"
+	"github.com/ethereum/go-ethereum/cmd/golembase/entity/list"
 	"github.com/ethereum/go-ethereum/cmd/golembase/entity/update"
 	"github.com/urfave/cli/v2"
 )
@@ -15,6 +16,7 @@ func Entity() *cli.Command {
 			create.Create(),
 			delete.Delete(),
 			update.Update(),
+			list.List(),
 		},
 	}
 }
