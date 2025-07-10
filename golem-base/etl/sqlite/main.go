e5109b4fdf2e5cc1d48dc18224175495ebfd1028
Dragan Milic <dragan@netice9.com>
2025-07-10 15:30:34 +0200

    extract ETL into it's own package

--- golem-base/etl/sqlite/main.go
+++ golem-base/etl/sqlite/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"database/sql"
-	_ "embed"
-	"errors"
 	"fmt"
 	"log/slog"
 	"math/big"
@@ -12,14 +9,11 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/golem-base/etl/sqlite/etl"
 	"github.com/ethereum/go-ethereum/golem-base/etl/sqlite/sqlitegolem"
 	"github.com/ethereum/go-ethereum/golem-base/wal"
-	_ "github.com/mattn/go-sqlite3"
 	"github.com/urfave/cli/v2"
 )
-
-//go:embed sqlitegolem/schema.sql
-var schema string
 
 func main() {
 	log := slog.New(slog.NewTextHandler(os.Stdout, nil))
@@ -54,31 +48,18 @@ func main() {
 			},
 		},
 		Action: func(c *cli.Context) error {
-
 			ctx, cancel := signal.NotifyContext(c.Context, os.Interrupt)
 			defer cancel()
 
-			db, err := sql.Open("sqlite3", fmt.Sprintf("file:%s?cache=shared&mode=rwc&_journal_mode=WAL", cfg.dbFile))
+			// Create ETL instance
+			etlInstance, err := etl.NewETL(cfg.dbFile)
 			if err != nil {
-				return fmt.Errorf("failed to open database: %w", err)
+				return fmt.Errorf("failed to create ETL instance: %w", err)
 			}
-			defer db.Close()
+			defer etlInstance.Close()
 
-			var tableName string
-			err = db.QueryRowContext(ctx, `
-				SELECT name FROM sqlite_master 
-				WHERE type='table' AND name='entities';
-			`).Scan(&tableName)
-
-			if err == sql.ErrNoRows {
-				log.Info("could not find 'entities' table, applying schema")
-				_, err := db.ExecContext(ctx, schema)
-				if err != nil {
-					return fmt.Errorf("failed to apply schema table: %w", err)
-				}
-			}
-
-			autocommit := sqlitegolem.New(db)
+			// Get queries instance for autocommit operations
+			autocommit := etlInstance.GetQueries()
 
 			ec, err := ethclient.Dial(cfg.rpcEndpoint)
 			if err != nil {
@@ -98,7 +79,7 @@ func main() {
 			if !hasProcessingStatus {
 				log.Info("no processing status found, inserting genesis block")
 
-				genesisHeade, err := ec.HeaderByNumber(ctx, big.NewInt(0))
+				genesisHeader, err := ec.HeaderByNumber(ctx, big.NewInt(0))
 				if err != nil {
 					return fmt.Errorf("failed to get genesis header: %w", err)
 				}
@@ -106,7 +87,7 @@ func main() {
 				err = autocommit.InsertProcessingStatus(ctx, sqlitegolem.InsertProcessingStatusParams{
 					Network:                  networkID.String(),
 					LastProcessedBlockNumber: 0,
-					LastProcessedBlockHash:   genesisHeade.Hash().String(),
+					LastProcessedBlockHash:   genesisHeader.Hash().String(),
 				})
 				if err != nil {
 					return fmt.Errorf("failed to insert processing status: %w", err)
@@ -126,146 +107,10 @@ func main() {
 					return fmt.Errorf("failed to iterate over wal: %w", err)
 				}
 
-				err = func() (err error) {
-					log.Info("processing block", "block", blockWal.BlockInfo.Number)
-					tx, err := db.BeginTx(ctx, nil)
-					if err != nil {
-						return fmt.Errorf("failed to begin transaction: %w", err)
-					}
-
-					defer func() {
-						if err != nil {
-							err = errors.Join(err, tx.Rollback())
-						}
-					}()
-
-					txDB := sqlitegolem.New(tx)
-
-					for op, err := range blockWal.OperationsIterator {
-						if err != nil {
-							return fmt.Errorf("failed to iterate over operations: %w", err)
-						}
-
-						switch {
-						case op.Create != nil:
-							log.Info("create", "entity", op.Create.EntityKey.Hex())
-							err = txDB.InsertEntity(ctx, sqlitegolem.InsertEntityParams{
-								Key:          op.Create.EntityKey.Hex(),
-								ExpiresAt:    int64(op.Create.ExpiresAtBlock),
-								Payload:      op.Create.Payload,
-								OwnerAddress: op.Create.Owner.Hex(),
-							})
-							if err != nil {
-								return fmt.Errorf("failed to insert entity: %w", err)
-							}
-
-							for _, annotation := range op.Create.NumericAnnotations {
-								err = txDB.InsertNumericAnnotation(ctx, sqlitegolem.InsertNumericAnnotationParams{
-									EntityKey:     op.Create.EntityKey.Hex(),
-									AnnotationKey: annotation.Key,
-									Value:         int64(annotation.Value),
-								})
-								if err != nil {
-									return fmt.Errorf("failed to insert numeric annotation: %w", err)
-								}
-							}
-
-							for _, annotation := range op.Create.StringAnnotations {
-								err = txDB.InsertStringAnnotation(ctx, sqlitegolem.InsertStringAnnotationParams{
-									EntityKey:     op.Create.EntityKey.Hex(),
-									AnnotationKey: annotation.Key,
-									Value:         annotation.Value,
-								})
-								if err != nil {
-									return fmt.Errorf("failed to insert string annotation: %w", err)
-								}
-							}
-						case op.Update != nil:
-							existingEntity, err := txDB.GetEntity(ctx, op.Update.EntityKey.Hex())
-							if err != nil {
-								return fmt.Errorf("failed to get existing entity: %w", err)
-							}
-
-							txDB.DeleteEntity(ctx, op.Update.EntityKey.Hex())
-							txDB.DeleteNumericAnnotations(ctx, op.Update.EntityKey.Hex())
-							txDB.DeleteStringAnnotations(ctx, op.Update.EntityKey.Hex())
-
-							txDB.InsertEntity(ctx, sqlitegolem.InsertEntityParams{
-								Key:          op.Update.EntityKey.Hex(),
-								ExpiresAt:    int64(op.Update.ExpiresAtBlock),
-								Payload:      op.Update.Payload,
-								OwnerAddress: existingEntity.OwnerAddress,
-							})
-
-							for _, annotation := range op.Update.NumericAnnotations {
-								err = txDB.InsertNumericAnnotation(ctx, sqlitegolem.InsertNumericAnnotationParams{
-									EntityKey:     op.Update.EntityKey.Hex(),
-									AnnotationKey: annotation.Key,
-									Value:         int64(annotation.Value),
-								})
-								if err != nil {
-									return fmt.Errorf("failed to insert numeric annotation: %w", err)
-								}
-							}
-
-							for _, annotation := range op.Update.StringAnnotations {
-								err = txDB.InsertStringAnnotation(ctx, sqlitegolem.InsertStringAnnotationParams{
-									EntityKey:     op.Update.EntityKey.Hex(),
-									AnnotationKey: annotation.Key,
-									Value:         annotation.Value,
-								})
-								if err != nil {
-									return fmt.Errorf("failed to insert string annotation: %w", err)
-								}
-							}
-						case op.Delete != nil:
-							err = txDB.DeleteEntity(ctx, op.Delete.Hex())
-							if err != nil {
-								return fmt.Errorf("failed to delete entity: %w", err)
-							}
-
-							err = txDB.DeleteNumericAnnotations(ctx, op.Delete.Hex())
-							if err != nil {
-								return fmt.Errorf("failed to delete numeric annotations: %w", err)
-							}
-
-							err = txDB.DeleteStringAnnotations(ctx, op.Delete.Hex())
-							if err != nil {
-								return fmt.Errorf("failed to delete string annotations: %w", err)
-							}
-						case op.Extend != nil:
-							log.Info("extend BTL", "entity", op.Extend.EntityKey.Hex())
-
-							// Update the entity with the new expiry time
-							err = txDB.UpdateEntityExpiresAt(ctx, sqlitegolem.UpdateEntityExpiresAtParams{
-								ExpiresAt: int64(op.Extend.NewExpiresAt),
-								Key:       op.Extend.EntityKey.Hex(),
-							})
-							if err != nil {
-								return fmt.Errorf("failed to extend entity BTL: %w", err)
-							}
-						}
-
-						log.Info("operation", "operation", op)
-					}
-
-					err = txDB.UpdateProcessingStatus(ctx, sqlitegolem.UpdateProcessingStatusParams{
-						Network:                  networkID.String(),
-						LastProcessedBlockNumber: int64(blockWal.BlockInfo.Number),
-						LastProcessedBlockHash:   blockWal.BlockInfo.Hash.Hex(),
-					})
-					if err != nil {
-						return fmt.Errorf("failed to insert processing status: %w", err)
-					}
-
-					return tx.Commit()
-
-				}()
-
+				err = etlInstance.InsertBlock(ctx, blockWal, networkID.String(), log)
 				if err != nil {
 					return fmt.Errorf("failed to process block: %w", err)
 				}
-
 			}
 
 			return nil
