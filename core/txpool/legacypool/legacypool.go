e821bbfa14d2a0f103fe5132daceffa7c1b47f62
Dragan Milic <dragan@netice9.com>
2025-11-05 12:00:34 +0100

    validate arkiv transactions on mempool level

--- core/txpool/legacypool/legacypool.go
+++ core/txpool/legacypool/legacypool.go
@@ -20,6 +20,7 @@ package legacypool
 import (
 	"context"
 	"errors"
+	"fmt"
 	"maps"
 	"math"
 	"math/big"
@@ -29,6 +30,7 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/ethereum/go-ethereum/arkiv/compression"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/prque"
 	"github.com/ethereum/go-ethereum/consensus/misc/eip1559"
@@ -659,8 +661,21 @@ func (pool *LegacyPool) ValidateTxBasics(tx *types.Transaction) error {
 		case to == nil:
 			return ErrNonGolembaseTransaction
 		case *to == address.GolemBaseStorageProcessorAddress:
+			if len(tx.Data()) == 0 {
+				return fmt.Errorf("golem base storage transaction data is empty")
+			}
+
 			return nil
 		case *to == address.ArkivProcessorAddress:
+			if len(tx.Data()) == 0 {
+				return fmt.Errorf("arkiv transaction data is empty")
+			}
+
+			_, err := compression.BrotliDecompress(tx.Data())
+			if err != nil {
+				return fmt.Errorf("failed to decompress arkiv transaction data: %w", err)
+			}
+
 			return nil
 
 		}
